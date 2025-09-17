743419c6fb90a224030a15fed864075d4a2d43b7
Dio10072003 <Dohhqe170246@fpt.edu.vn>
2025-09-17 07:13:00 +0700

    feat: Initialize Product entity and DBContext

---
+++ Domain/Context/DBContext.cs
@@ -0,0 +1,363 @@
+﻿using Domain.Domain.Entities;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Context;
+
+public partial class DBContext : DbContext
+{
+    public DBContext()
+    {
+    }
+
+    public DBContext(DbContextOptions<DBContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Blog> Blogs { get; set; }
+
+    public virtual DbSet<Cart> Carts { get; set; }
+
+    public virtual DbSet<CartItem> CartItems { get; set; }
+
+    public virtual DbSet<Comment> Comments { get; set; }
+
+    public virtual DbSet<FavoriteProduct> FavoriteProducts { get; set; }
+    public virtual DbSet<BankSettings> BankSettings { get; set; }
+
+    public virtual DbSet<LikeBlog> LikeBlogs { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<ProductFeedback> ProductFeedbacks { get; set; }
+
+    public virtual DbSet<Shop> Shops { get; set; }
+
+    public virtual DbSet<Transaction> Transactions { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+    public virtual DbSet<ProductImages> ProductImages { get; set; }
+
+    public virtual DbSet<Advertisement> Advertisements { get; set; }
+
+    public virtual DbSet<AdvertisementTracking> AdvertisementTrackings { get; set; }
+
+    public virtual DbSet<Slider> Sliders { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Blog>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.ModifyDate).HasColumnType("datetime");
+            entity.Property(e => e.PublishDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.AuthorNavigation).WithMany(p => p.Blogs)
+                .HasForeignKey(d => d.Author)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK_Blogs_Users");
+        });
+
+        modelBuilder.Entity<Cart>(entity =>
+        {
+            entity.ToTable("Cart");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.TotalAmounts).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Carts)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK_Cart_Users");
+        });
+
+        modelBuilder.Entity<CartItem>(entity =>
+        {
+            entity.ToTable("CartItem");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.CartId).HasColumnName("CartID");
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.TotalAmounts).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.UnitPrice)
+                .HasMaxLength(10)
+                .IsFixedLength();
+
+            entity.HasOne(d => d.Cart).WithMany(p => p.CartItems)
+                .HasForeignKey(d => d.CartId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK_CartItem_Cart");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.CartItems)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK_CartItem_Products");
+        });
+
+        modelBuilder.Entity<Comment>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.BlogId).HasColumnName("BlogID");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifyDate).HasColumnType("datetime");
+            entity.Property(e => e.ParentId).HasColumnName("ParentID");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.Blog).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.BlogId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK_Comments_Blogs");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_Comments_Users");
+        });
+
+        modelBuilder.Entity<FavoriteProduct>(entity =>
+        {
+            entity.ToTable("FavoriteProduct");
+
+            // Composite primary key
+            entity.HasKey(e => new { e.UserId, e.ProductId });
+
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.Product)
+                .WithMany(p => p.FavoriteProducts)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            entity.HasOne(d => d.User)
+                .WithMany(u => u.FavoriteProducts)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+
+
+
+
+        modelBuilder.Entity<LikeBlog>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.BlogId).HasColumnName("BlogID");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.Blog).WithMany(p => p.LikeBlogs)
+                .HasForeignKey(d => d.BlogId)
+                .HasConstraintName("FK_LikeBlogs_Blogs");
+
+            entity.HasOne(d => d.User).WithMany(p => p.LikeBlogs)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_LikeBlogs_Users");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.DeliveryAddress)
+                .HasMaxLength(10)
+                .IsFixedLength();
+                entity.Property(e => e.ModifyDate).HasColumnType("datetime");
+            entity.Property(e => e.Status).HasMaxLength(255);
+            entity.Property(e => e.TotalAmounts).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_Orders_Users");
+            entity.Property(x => x.PaymentMethod)
+                  .HasConversion<string>();
+        });
+
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.OrderId).HasColumnName("OrderID");
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+                entity.Property(e => e.TotalAmounts).HasColumnType("decimal(18, 0)");
+                entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 0)");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.OrderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_OrderDetails_Orders");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_OrderDetails_Products");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Category).HasMaxLength(255);
+            entity.Property(e => e.CostPrice).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifyDate).HasColumnType("datetime");
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.ShopID).HasColumnName("ShopID");
+            entity.Property(e => e.Status).HasMaxLength(255);
+
+            entity.HasOne(d => d.Shop).WithMany(p => p.Products)
+                .HasForeignKey(d => d.ShopID)
+                .HasConstraintName("FK_Products_Shops");
+        });
+
+        modelBuilder.Entity<ProductFeedback>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifyDate).HasColumnType("datetime");
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.Rating).HasColumnType("decimal(18, 0)");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.ProductFeedbacks)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK_ProductFeedbacks_Products");
+
+            entity.HasOne(d => d.User).WithMany(p => p.ProductFeedbacks)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_ProductFeedbacks_Users");
+        });
+
+        modelBuilder.Entity<Shop>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Address).HasMaxLength(255);
+            entity.Property(e => e.City).HasMaxLength(255);
+            entity.Property(e => e.CoverImageUrl).HasMaxLength(500);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.LogoUrl).HasMaxLength(255);
+            entity.Property(e => e.ModifyDate).HasColumnType("datetime");
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.Phone).HasMaxLength(10);
+            entity.Property(e => e.Province).HasMaxLength(255);
+            entity.Property(e => e.Qrbanking)
+                .HasMaxLength(500)
+                .IsFixedLength()
+                .HasColumnName("QRBanking");
+            entity.Property(e => e.RatingAverage).HasColumnType("decimal(18, 0)");
+
+            entity.HasOne(d => d.Owner).WithMany(p => p.Shops)
+                .HasForeignKey(d => d.OwnerId)
+                .HasConstraintName("FK_Shops_Users");
+        });
+
+        modelBuilder.Entity<Transaction>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Expired)
+                .IsRowVersion()
+                .IsConcurrencyToken();
+            entity.Property(e => e.OrderId).HasColumnName("OrderID");
+            entity.Property(e => e.TotalAmounts).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.Transactions)
+                .HasForeignKey(d => d.OrderId)
+                .HasConstraintName("FK_Transactions_Orders");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Transactions)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_Transactions_Users");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Address).HasMaxLength(50);
+            entity.Property(e => e.Avatar).HasMaxLength(255);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Dbo).HasColumnName("DBO");
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.FullName).HasMaxLength(255);
+            entity.Property(e => e.Gender).HasMaxLength(50);
+            entity.Property(e => e.ModifyDate).HasColumnType("datetime");
+            entity.Property(e => e.Password).HasMaxLength(255);
+            entity.Property(e => e.Phone).HasMaxLength(50);
+            entity.Property(e => e.Role).HasMaxLength(50);
+            entity.Property(e => e.UserName).HasMaxLength(50);
+            entity.Property(e => e.Role).HasConversion<string>();
+        });
+
+        modelBuilder.Entity<Advertisement>(entity =>
+        {
+            entity.HasKey(e => e.ID);
+            entity.Property(e => e.ID)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.MediaUrl).HasColumnType("nvarchar(max)");
+            entity.Property(e => e.RedirectUrl).HasColumnType("nvarchar(max)");
+            entity.Property(e => e.StartDate).HasColumnType("datetime");
+            entity.Property(e => e.EndDate).HasColumnType("datetime");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifyDate).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<AdvertisementTracking>(entity =>
+        {
+            entity.HasKey(e => e.ID);
+            entity.Property(e => e.ID)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.AdvertisementID).HasColumnName("AdvertisementID");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.TrackingType).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Slider>(entity =>
+        {
+            entity.HasKey(e => e.ID);
+            entity.Property(e => e.ID)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Description).HasColumnType("nvarchar(max)");
+            entity.Property(e => e.ImageUrl).HasColumnType("nvarchar(max)");
+            entity.Property(e => e.LinkUrl).HasColumnType("nvarchar(max)");
+            entity.Property(e => e.StartDate).HasColumnType("datetime");
+            entity.Property(e => e.EndDate).HasColumnType("datetime");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifyDate).HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Entities;
+
+namespace Domain.Domain.Entities;
+
+public partial class Product
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public decimal CostPrice { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Stock { get; set; }
+
+    public string Material { get; set; } = null!;
+    public string CommonImage { get; set; } = null!;
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime? ModifyDate { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    public string Category { get; set; } = null!;
+
+    public virtual ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+    public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+
+    public virtual ICollection<ProductFeedback> ProductFeedbacks { get; set; } = new List<ProductFeedback>();
+    public virtual ICollection<ProductImages>? ProductImages { get; set; }
+    public virtual ICollection<FavoriteProduct> FavoriteProducts { get; set; } = new List<FavoriteProduct>();
+
+
+    [Column("ShopID")]
+    public Guid? ShopID { get; set; }
+
+    public virtual Shop? Shop { get; set; }
+}
