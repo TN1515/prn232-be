e6df41f57e625162f3f14ff94448ff52c830ff31
HuyNLH <nglehuuhuy04@gmail.com>
2025-09-18 15:20:11 +0700

    feat: Create IUserRepository interface

---
+++ Application/Interface/IRepository/V1/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Payload.Base;
+using Domain.Payload.Base.Paginate;
+using Domain.Payload.Response.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interface.IRepository.V1
+{
+    public interface IUserRepository
+    {
+        Task<ApiResponse<string>> BlockOrUnBlock(Guid userId);
+        Task<ApiResponse<ProcedurePagingResponse<GetUsersResponse>>> GetUsers(int pageNumber,int pageSize, string?search, string?filter);
+    }
+}
