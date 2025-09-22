06956a0bec72df938469494bcf9782a033d5633b
Dio10072003 <Dohhqe170246@fpt.edu.vn>
2025-09-22 15:39:41 +0700

    feat: Initialize Blog and Comment entities

---
+++ Domain/Entities/Blog.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Domain.Entities;
+
+public partial class Blog
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Content { get; set; } = null!;
+
+    public string Image { get; set; } = null!;
+
+    public Guid Author { get; set; }
+
+    public DateTime PublishDate { get; set; }
+
+    public DateTime? ModifyDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public virtual User AuthorNavigation { get; set; }
+
+    public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+    public virtual ICollection<LikeBlog> LikeBlogs { get; set; } = new List<LikeBlog>();
+}
---
+++ Domain/Entities/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Domain.Entities;
+
+public partial class Comment
+{
+    public Guid Id { get; set; }
+
+    public Guid? UserId { get; set; }
+
+    public string Content { get; set; } = null!;
+
+    public Guid? ParentId { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public DateTime ModifyDate { get; set; }
+
+    public Guid? BlogId { get; set; }
+
+    public virtual Blog? Blog { get; set; }
+
+    public virtual User? User { get; set; }
+}
