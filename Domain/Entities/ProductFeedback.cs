9566edb4393482df3cccc09d3cef8c374330d4c8
platypuss2303 <nguyengialuatace@gmail.com>
2025-09-25 17:02:00 +0700

    feat: Initialize ProductFeedback entity

---
+++ Domain/Entities/ProductFeedback.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain.Domain.Entities;
+
+public partial class ProductFeedback
+{
+    public Guid Id { get; set; }
+
+    public decimal Rating { get; set; }
+
+    public string? Content { get; set; }
+
+    public Guid? UserId { get; set; }
+
+    public Guid? ProductId { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime ModifyDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public virtual Product? Product { get; set; }
+
+    public virtual User? User { get; set; }
+}
