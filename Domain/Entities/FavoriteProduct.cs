ca2bb316ab78d70f7c65aa801aa6659a75e8ba29
TN1515 <tmb14112004@gmail.com>
2025-09-26 09:58:47 +0700

    feat: Initialize FavoriteProduct and LikeBlog entities

---
+++ Domain/Entities/FavoriteProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Domain.Entities;
+
+public partial class FavoriteProduct
+{
+    public Guid ProductId { get; set; }
+    public Guid UserId { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+    public virtual User User { get; set; } = null!;
+
+}
---
+++ Domain/Entities/LikeBlog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Domain.Entities;
+
+public partial class LikeBlog
+{
+    public Guid Id { get; set; }
+
+    public Guid? UserId { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public Guid? BlogId { get; set; }
+
+    public virtual Blog? Blog { get; set; }
+
+    public virtual User? User { get; set; }
+}
