26ed5c0352dc7cb211336147a5ebe89ec9f87edd
platypuss2303 <nguyengialuatace@gmail.com>
2025-09-27 13:07:00 +0700

    feat: Create LikeBlogController for likes and favorites

---
+++ API/Controllers/V1/LikeBlogController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interface.IRepository.V1;
+using Domain.Payload.Request.Like;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.V1
+{
+    [ApiController]
+    [Route("api/v1/likes")]
+    public class LikeBlogController(ILikeRepository _repository,
+                                    ILogger<LikeBlogController> _logger) : Controller
+    {
+        [HttpPost("like-dislike")]
+        [Authorize]
+        public async Task<IActionResult> LikeBlog([FromBody] LikeRequest request)
+        {
+
+            try
+            {
+                var response = await _repository.LikeOrDislike(request);
+                return StatusCode(response.StatusCode, response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("[Like API]" + "\n" + ex.ToString());
+                return StatusCode(500, ex.InnerException?.Message);
+            }
+
+        }
+    }
+}
