340cb76332d2b426f7f8f3d0932a68cc7f1a14f3
HuyNLH <nglehuuhuy04@gmail.com>
2025-09-29 20:10:35 +0700

    feat: Initialize BankSettings and Transaction entities

---
+++ Domain/Entities/BankSettings.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Domain.Entities;
+
+namespace Domain.Entities
+{
+    public class BankSettings
+    {
+        public Guid ID { get; set; }
+        public string BankName { get; set; }
+        public string BankNo { get; set; }
+        public bool IsUse { get; set; }
+        public Guid ShopID { get; set; }
+        public virtual Shop Shop { get; set; }
+    }
+}
---
+++ Domain/Entities/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Domain.Entities;
+
+public partial class Transaction
+{
+    public Guid Id { get; set; }
+
+    public string Url { get; set; } = null!;
+
+    public byte[] Expired { get; set; } = null!;
+
+    public decimal TotalAmounts { get; set; }
+
+    public Guid? UserId { get; set; }
+
+    public Guid? OrderId { get; set; }
+
+    public virtual Order? Order { get; set; }
+
+    public virtual User? User { get; set; }
+}
