b1c60893da0e812039a0e30a0e1498a8154c1690
dinhthg2 <dinhthgqe170222@fpt.edu.vn>
2025-10-01 06:52:35 +0700

    feat: Initialize ProductImages entity

---
+++ Domain/Entities/ProductImages.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Domain.Entities;
+
+namespace Domain.Entities
+{
+    public class ProductImages
+    {
+        public Guid ID { get; set; }
+        public string Url { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Guid ProductID { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
