a9833217bc73f59f291bd51b922ecd716b06efce
platypuss2303 <nguyengialuatace@gmail.com>
2025-10-22 10:32:17 +0700

    feat: Setup RabbitMQ configuration

---
+++ RabibtMQ/Config/RabbitMQConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace RabbitMQContract.Config
+{
+    public class RabbitMQConfig
+    {
+        public string HostName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Port { get; set; } = 5672;
+        public string VirtualHost { get; set; }
+    }
+}
