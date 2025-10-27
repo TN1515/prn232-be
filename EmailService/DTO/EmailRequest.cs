c598e2ec5db24e86ce9b30c297ca4043b46641ad
Dio10072003 <Dohhqe170246@fpt.edu.vn>
2025-10-27 23:11:28 +0700

    feat: Create email service DTOs

---
+++ EmailService/DTO/EmailRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService.DTO
+{
+    public class EmailRequest<T>
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public T Body { get; set; }
+    }
+}
