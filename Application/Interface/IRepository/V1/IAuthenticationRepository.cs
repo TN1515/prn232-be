f4c6687d8cebd9bc15fba415b4b9d41e8ba2eb18
platypuss2303 <nguyengialuatace@gmail.com>
2025-11-02 09:23:55 +0700

    feat: Implement all repositories and API controllers with endpoints

---
+++ Application/Interface/IRepository/V1/IAuthenticationRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Payload.Base;
+using Domain.Payload.Request.Auth;
+using Domain.Payload.Request.Users;
+using Domain.Payload.Response.Auth;
+using Domain.Payload.Response.Users;
+
+namespace Application.Interface.IRepository.V1
+{
+    public interface IAuthenticationRepository
+    {
+        Task<ApiResponse<string>> UserRegister(RegisterRequest request);
+        Task<ApiResponse<LoginResponse>> Login(LoginRequest request);
+        Task<ApiResponse<string>> ChangePassword(ChangePasswordRequest request);
+        Task<ApiResponse<string>> SendKeyForgotPassword(SendKeyEmailRequest request);
+        Task<ApiResponse<string>> SetNewPassword(SetNewPasswordRequest request);
+        Task<ApiResponse<GetUserResponse>> GetUser();
+        Task<ApiResponse<string>> EditProfile(EditUserRequest request);
+    }
+}
